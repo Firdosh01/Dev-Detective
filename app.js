b200d765b71ee73bb8d2f536612682b67625acb2
Firdosh01 <firdosh7392@gmail.com>
2023-03-19 07:34:44 -0500

    little update

---
+++ app.js
@@ -0,0 +1,19 @@
+//Variables
+
+const btnmode = document.querySelector("#btn-mode");
+const modetext = document.querySelector("#mode-text");
+const modeicon = document.querySelector("#mode-icon");
+const input = document.querySelector("#input");
+const btnsubmit = document.querySelector("#submit");
+const avatar = document.querySelector("#avatar");
+const userName = document.querySelector("#name");
+const user = document.querySelector("#user");
+const date = document.querySelector("#date");
+const bio = document.querySelector("#bio");
+const repos = document.querySelector("#repos");
+const followers = document.querySelector("#followers");
+const following = document.querySelector("#following");
+const user_location = document.querySelector("#location");
+const page = document.querySelector("#page");
+const twitter = document.querySelector("#twitter");
+const company = document.querySelector("#company");
